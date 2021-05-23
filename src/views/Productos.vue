64ff1e1961693761f8f0edc12f148abaec3e77d7
sebastianmauriz@gmail.com <sebastianmauriz@gmail.com>
2021-05-23 18:45:22 -0300

    agrego componentes

---
+++ src/views/Productos.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>producto works</p>
+    <b-button href="/">volver</b-button>
+  </div>
+</template>
+
+
+<script>
+export default {};
+</script>
---
+++ src/views/Ubicacion.vue
@@ -0,0 +1,10 @@
+<template>
+<div>
+  <p>ubicacion works</p>
+  <b-button href="/">VOLVER</b-button >             
+</div>
+
+</template>
+<script>
+export default {};
+</script>
